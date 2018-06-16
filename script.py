11574f7e6b08050efdb416f90fed35746be213af
Pirvanescu Livius <livius@192-168-0-138.rdsnet.ro>
2018-06-16 13:08:31 +0300

    Citire serie mesaje

--- script.py
+++ script.py
@@ -6,16 +6,17 @@ API_KEY = "mfEFDGHIALHCwJSDLx5uTsBrxtmn1Ol2yqkz1uIk7bI"
 
 def main():
 	paralleldots.set_api_key(API_KEY)
-
+	messages = []
 	with open("messages.csv") as csv_file:
 		reader = csv.reader(csv_file, delimiter='|')
 		for row in reader:
-			message = row[2]
+			messages.append(row[2])
 
-	response = paralleldots.language_detection(message)
-	language = response['output']
+	#response = paralleldots.language_detection(message)
+	#language = response['output']
 
-	print language    
+	messages = messages[1:]
+	print messages    
 
 if __name__ == '__main__':
     main()
